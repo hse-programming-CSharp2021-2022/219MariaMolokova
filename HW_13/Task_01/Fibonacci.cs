c1e1fbd10ad4fc0b596b2c1f4130b7adc9ac2229
Maria <71029434+naborbukovok@users.noreply.github.com>
2022-03-26 09:59:59 +0300

    7-14

---
+++ HW_13/Task_01/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    public class Fibonacci
+    {
+        public IEnumerable<int> GetNums(int n)
+        {
+            var f1 = 1;
+            var f2 = 1;
+            for (var i = 0; i < n; i++)
+            {
+                (f1, f2) = (f2, f1 + f2);
+                yield return f1;
+            }
+        }
+    }
+}
